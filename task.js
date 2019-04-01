c4b4b2048a954f716749340a98f69d34fa596caf
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-04-01 18:18:37 +0300

    fixed bugs

--- task.js
+++ task.js
@@ -1,14 +1,15 @@
-function compareObjects(firstObject, secondObject) {
-    return firstObject.id === secondObject.id && firstObject.name === secondObject.name === -1
-}
 function mergeArray(firstArray, secondArray) {
     'use strict';
     const tmpArray = firstArray.concat(secondArray);
-    const resArray = [];
-    tmpArray.forEach(sourceItem => {
-        if (resArray.findIndex(resItem => compareObjects(sourceItem, resItem))) {
-            resArray.push(sourceItem);
-        }
-    });
-    return resArray;
+    const resArray = new Map();
+    tmpArray.forEach(item => resArray.set(item.id, item));
+    return Array.from(resArray.values());
 }
+
+function test() {
+    const arr1 = [{id:1, name: 'foo'}, {id:2, name: 'bar'}];
+    const arr2 = [{id:3, name: 'bla'}, {id:1, name:'foo'}, {id:4, name:'baz'}];
+    console.log(mergeArray(arr1, arr2));
+}
+test();
+
